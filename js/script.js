e4f208cccd8a238dc2753cc0c479058b75ee8e62
MrGians <gianlumura97@gmail.com>
2022-07-02 23:00:52 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -8,36 +8,10 @@
 // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero totalCell - bomb = n ).
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-// # MILESTONE 1
-// Prepariamo "qualcosa" per tenere il punteggio dell'utente.
-// Quando l'utente clicca su una cella, incrementiamo il punteggio.
-// Se riusciamo, facciamo anche in modo da non poter più cliccare la stessa cella.
-
-// # MILESTONE 2
-// Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
-// Generiamoli e stampiamo in console per essere certi che siano corretti
-
-// # MILESTONE 3
-// Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba, controllando se il numero di cella è presente nell'array di bombe. Se si, la cella diventa rossa (raccogliamo il punteggio e e scriviamo in console che la partita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
-
-// # MILESTONE 4
-// Quando l'utente clicca su una cella, e questa non è una bomba, dobbiamo controllare se il punteggio incrementato ha raggiunto il punteggio massimo perchè in quel caso la partita termina. Raccogliamo quindi il messaggio è scriviamo un messaggio appropriato.
-// (Ma come stabiliamo quale sia il punteggio massimo?)
-
-// # MILESTONE 5
-// Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba o se perchè l'utente ha raggiunto il punteggio massimo. Dobbiamo poi stampare in pagina il punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta.
-
-// #BONUS:
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
 
 
 // Variabili Globali DOM
 const playBtn = document.getElementById("btn-play");
-
 
 
 // Renderizzo la griglia e creo N elementi Cella (in base alla difficoltà scelta) al click del bottone "Play"
@@ -47,6 +21,7 @@ playBtn.addEventListener("click", () => {
   const gridChoice = document.getElementById("grid-choice");
   const userPointsCounter = document.getElementById("user-points-counter");
 
+// ------------------------------------- FUNCTION ------------------------------------- //
   // Creo una funzione che generi le celle con la relativa classe
   const createCell = (totalCells) => {
     // Creo l'elemento Cella
@@ -61,47 +36,99 @@ playBtn.addEventListener("click", () => {
     return cell;
   };
 
-  // Creo una funzione che generi i numeri che diventeranno le bombe
-  const createBomb = (min, max) => {
-    max++;
-    return Math.floor(Math.random() * (max - min)) + min;
+  // Creo una funzione che generi le bombe
+  const createBombs = (min, max) => {
+
+    // Creo un array che andrà a contenere il totale delle bombe
+    const bombsList = [];
+
+    // Genero le bombe tramite un ciclo FOR
+    for (let i = 1; i <= 16; i++) {
+      // Creo un numero random da assegnare alla Bomba
+      let randomBombNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
+
+      // SE il numero Bomba creato è già presente nell'array ripeto il giro
+      if (bombsList.includes(randomBombNumber))  {i--;}
+      // SE il numero Bomba creato NON è presente nell'array lo aggiungo e proseguo
+      else {bombsList.push(randomBombNumber);}
+    }
+
+    // Restituisco un array contenente il totale delle bombe
+    return bombsList;
   };
+
+  // Creo una funzione che gestisca la difficoltà e di conseguenza le celle generate
+  const chooseDifficulty = (selection) => {
+
+    // Creo variabili di appoggio per la scelta della difficoltà in griglia (Default: Easy)
+    let rows = 10;
+    let cells = 10;
+
+    // Modifico variabili di appoggio per la griglia in base alla difficoltà selezionata
+    switch (selection.value) {
+    // Se viene selezionata l'opzione "Hardcore"
+    case "hard":
+      rows = cells = 7;
+      break;
+      
+    // Se viene selezionata l'opzione "Classic"
+    case "classic":
+      rows = cells = 9;
+      break;
+
+      // Se viene selezionata l'opzione "Easy" (Default)
+    case "easy":
+      default:
+      rows = cells = 10;
+      break;
+    }
+
+    // Determino il numero totale finale di Celle
+    const totalCells = rows * cells; // Default: Easy
+    // Restituisco il valore di totalCells
+    return totalCells;
+  };
+
+  // Creo una funzione che gestisca il messaggio di Vittoria / Game-over
+  const hasWon = (hasWon = false) => {
+
+    // Creo l'elemento messaggio e gli assegno la classe game-message
+    const gameMessage = document.createElement("div");
+    gameMessage.classList.add("game-message");
+    
+    // Se ha vinto Genero messaggio di Vittoria del giocatore
+    if (hasWon) {gameMessage.innerHTML = `<h2>COMPLIMENTI! HAI VINTO!</h2>`;}
+    // Se ha perso Genero messaggio di Game-Over
+    else {gameMessage.innerHTML = `<h2>GAME OVER!</h2>`;}
+
+    // Restituisco il messaggio finale
+    return gameMessage;
+  }
+
+  // Creo una funzione per mostrare tutte le bombe sulla griglia al Game-Over
+  const showAllBombs = (totalCells, bombsList, cellElements) => {
+    // Confronto ogni numero cella con ogni numero bomba dell'array Bombs
+    for (let i = 1; i <= totalCells; i++) {
+      for (let j = 0; j < bombsList.length; j++) {
+        // Se il numero della corrisponde al numero bomba diventa rossa
+        if (i === bombsList[j]) {cellElements[bombsList[j] - 1].classList.add("clicked", "bomb");}
+      }
+    }
+  }
+
+// ------------------------------------- MINEFIELD GAME ------------------------------------- //
 
   // Il Bottone "Play" diventa "Ricomincia"
   playBtn.innerText = "Ricomincia";
 
   // Rendo visibile la griglia
   grid.classList.remove("d-none");
-  userPointsCounter.classList.remove("d-none");
   
   // Resetto gli elementi <div> dentro alla griglia (Utilità dal secondo click di "play" in poi)
   grid.innerHTML = "";
   
-  // Creo variabili di appoggio per la griglia (Default: Easy)
-  let rows = 10;
-  let cells = 10;
-
-  // Modifico variabili di appoggio per la griglia in base alla difficoltà selezionata
-  switch (gridChoice.value) {
-    // Se viene selezionata l'opzione "Hardcore"
-    case "hard":
-      rows = cells = 7;
-      break;
-
-    // Se viene selezionata l'opzione "Classic"
-    case "classic":
-      rows = cells = 9;
-      break;
-
-    // Se viene selezionata l'opzione "Easy" (Default)
-    case "easy":
-    default:
-      rows = cells = 10;
-      break;
-  }
-
-  // Determino il numero totale finale di Celle
-  let totalCells = rows * cells; // Default: Easy
+  // Restituisco il corrispettivo di celle in base alla difficoltà scelta
+  const totalCells = chooseDifficulty(gridChoice);
 
   // Creo una variabile che tenga conto del punteggio dell'utente durante il gioco
   let userPoints = 0;
@@ -110,7 +137,7 @@ playBtn.addEventListener("click", () => {
 
   // Genero le celle tramite un ciclo FOR
   for (let i = 1; i <= totalCells; i++) {
-  
+    // Creo la cella tramite la funzione createCell
     const cell = createCell(totalCells);
 
     // Inserisco un numero ad ogni cella
@@ -119,87 +146,56 @@ playBtn.addEventListener("click", () => {
     // Aggiungo evento al click della Cella
     cell.addEventListener("click", (event) => {
 
-    // Recupero gli elementi <div> cella dalla griglia
-    const cellElements = [...grid.querySelectorAll("div")];  
+      // Recupero gli elementi <div> cella dalla griglia
+      const cellElements = [...grid.querySelectorAll("div")];  
 
-      // Verifico che la cella non sia già stata cliccata
-      if (event.target.classList.contains("clicked")) {
-        return;
-      } else {
-        // Aggiungo la classe "Clicked" alla cella
+      // SE la cella è già stata cliccata interrompe l'azione bloccando eventuali eventi al click
+      if (event.target.classList.contains("clicked")) {return;}
+      // // SE la cella non è stata ancora cliccata
+      else {
+        // Aggiungo la classe "Clicked" alla cella e ne stampo il numero in console
         event.target.classList.add("clicked");
-        // Stampo il numero della cella cliccata in console
         console.log("Cell: " + event.target.innerText);
         
-        // Verifico se l'utente ha cliccato una Bomba o meno
+        // SE l'utente ha cliccato una Bomba
         if (bombs.includes(parseInt(event.target.innerText))) {
+
+          // Aggiungo la classe "bomb" alla cella
           event.target.classList.add("bomb");
-          console.log("BOMBA, GAME-OVER")
-        } else {
+          // Rendo visibili le caselle Bomba rimanenti sulla griglia
+          const showBombs = showAllBombs(totalCells, bombs, cellElements);
+          // Genero messaggio di Game-Over e la partita finisce
+          grid.appendChild(hasWon(false));
+          // Cambio il testo al Bottone "Play"
+          playBtn.innerText = "Gioca ancora";
+
+        }
+        // SE l'utente NON ha cliccato una bomba
+        else {
+          // Aggiungo la classe "safe" alla cella
           event.target.classList.add("safe");
-          console.log("SAFE")
-          // Aumento di 1 il punteggio utente
+          // Incremento di 1 il punteggio dell'utente e stampo il risultato
           userPoints++;
           userPointsCounter.innerText = `Punteggio: ${userPoints}`;
         }
 
         // SE l'utente ha raggiunto il punteggio massimo la partita finisce
         if (userPoints === totalCells - bombs.length) {
-        
           // Genero messaggio di Vittoria del giocatore
-          const gameMessage = document.createElement("div");
-          gameMessage.classList.add("game-message");
-          gameMessage.innerHTML = `<h2>COMPLIMENTI! HAI VINTO!</h2>`;
-          grid.appendChild(gameMessage);
-
-          // Cambio il testo al Bottone "Play"
-          playBtn.innerText = "Gioca ancora";
-        }
-        // SE l'utente ha cliccato una bomba la partita finisce
-        else if (event.target.classList.contains("bomb")) {
-          
-          // Rendo visibili al Game Over le caselle Bomba rimaste sulla griglia
-          for (let i = 1; i <= totalCells; i++) {
-            // Confronto ogni numero cella con ogni numero bomba dell'array Bombs
-            for (let j = 0; j < bombs.length; j++) {
-              // Se il numero della corrisponde al numero bomba diventa rossa
-              if (i === bombs[j]) {cellElements[bombs[j] - 1].classList.add("clicked", "bomb");}
-            }
-          }
-
-          // Genero messaggio di Game-Over
-          const gameMessage = document.createElement("div");
-          gameMessage.classList.add("game-message");
-          gameMessage.innerHTML = `<h2>GAME OVER!</h2>`;
-          grid.appendChild(gameMessage);
-
+          grid.appendChild(hasWon(true));
           // Cambio il testo al Bottone "Play"
           playBtn.innerText = "Gioca ancora";
         }
       }
 
     });
-
+    
     // Inserisco le celle all'interno della griglia
     grid.appendChild(cell);
   }
 
-
-  // Genero le bombe tramite un ciclo FOR e le inserisco nell'Array Bombs
-  const bombs = [];
-  for (let i = 1; i <= 16; i++) {
-
-    // Creo il numero della "Bomba"
-    let bomb = createBomb(1, totalCells);
-
-    // SE il numero Bomba è già presente nell'array, ripeto il giro, altrimenti lo aggiungo
-    if (bombs.includes(bomb))  {i--;} 
-    else {
-      bombs.push(bomb);
-      console.log(bombs);  
-    }
-  }
-
-
+  // Genero le bombe tramite la funzione createBombs e le stampo in console
+  const bombs = createBombs(1, totalCells);
+  console.log(bombs);
 });
 
